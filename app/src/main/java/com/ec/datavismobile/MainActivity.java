5a98ef691d5359cc5ba8d9696c9dae23fde4bfb1
probably-not-porter <pelibby16@earlham.edu>
2020-01-13 19:57:02 -0500

    set up web view

--- app/src/main/java/com/ec/datavismobile/MainActivity.java
+++ app/src/main/java/com/ec/datavismobile/MainActivity.java
@@ -1,14 +1,26 @@
 package com.ec.datavismobile;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webview =(WebView)findViewById(R.id.webView);
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl("http://cluster.earlham.edu:9900/");
+
     }
 }
